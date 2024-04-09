3cddd8787c821122c633e6728c040734805fd8c1
alejo-iiii <157398131+alejo-iiii@users.noreply.github.com>
2024-04-08 22:49:18 -0600

    main.js

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
 const form = document.querySelector("#loginForm");
 
-// Event listener for the form submission
 form.addEventListener('submit', (e) => {
-    e.preventDefault(); // Prevents the default form submission behavior
+    e.preventDefault();
 
-    // Get the values entered by the user
+    // obtener valores input
     const userInput = form.querySelector("[name='userInput']").value;
     const passInput = form.querySelector("[name='passInput']").value;
 
-    // Check if the user exists in the accounts array
+    // validar credenciales
     const user = accounts.find(account => account.userMail === userInput && account.userPass.toString() === passInput);
 
     if (user) {
-        // Save the logged-in user's data to local storage
+        // guardar en local storage lod datos del usuario
         localStorage.setItem("loggedInUser", JSON.stringify(user));
 
-        // Redirect to the home page
+        // redireccionar
         window.location.href = "home.html"; // Change "home.html" to the URL of your home page
     } else {
-        // Display an error message if authentication fails
         alert("Invalid username or password");
     }
 });
